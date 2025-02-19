f99cbcb2820b75a88e54bd2a88d70129f5d2779a
samuka7abr <samuelabrao2006@gmail.com>
2025-02-19 14:41:09 -0300

    feat: user register working

--- MoovieNotes/src/pages/SignUp/index.jsx
+++ MoovieNotes/src/pages/SignUp/index.jsx
@@ -1,13 +1,41 @@
+import { api } from '../../../../../MoviesNotesAPI/src/services/api';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Container, Form } from './styles';
-import { Link } from 'react-router-dom';
 import { Background } from './styles';
-
-
+import { useState } from 'react';
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function handleSignUp(){
+    if(!name || !email || !password){
+      return alert('Preencha todos os campos!');
+    }
+
+    api.post('/users', { name, email, password })
+    .then(() => {
+      alert('Usuário cadastrado com sucesso!');
+      navigate('/');
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message);
+      }
+      else{
+        alert('Não foi possível cadastrar');
+      }
+    })
+
+  }
+
+
   return (
     <Container>
       <Background />
@@ -21,22 +49,25 @@ export function SignUp() {
           placeholder="Nome"
           type="name"
           icon={FiUser}
+          onChange = {e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange = {e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange = {e => setPassword(e.target.value)}
         />
         
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">Voltar para o LogIn</Link>
 
